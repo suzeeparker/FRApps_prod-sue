ce23faba4c01482df002e8875732f15010f1fd93
Robin Mattern <robin.mattern@gmail.com>
2022-03-15 11:15:25 -0400

    Update deploy.sh
    Updated it, but didn't test it

--- client/5c-my-react-app/deploy.sh
+++ client/5c-my-react-app/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-   aHost=vultr-formr0-nimda;
-                            if [ "$1" != "" ]; then aHost=$1;    fi
-   aWebDir=/webs/FRApps;    if [ "$2" != "" ]; then aWebDir=$2=; fi
-   aBuild=build;            if [ "$3" != "" ]; then aBuild=$3;   fi
+    aHost=vultr-formr0-nimda;
+                             if [ "$1" != "" ]; then aHost=$1;    fi
+    aWebDir=/webs/FRApps;    if [ "$2" != "" ]; then aWebDir=$2=; fi
+    aBuild=build;            if [ "$3" != "" ]; then aBuild=$3;   fi
 
  if [ "${aHost}" == "" ]; then
 
@@ -20,22 +20,20 @@
     exit
     fi
 
- aCurDir=$( dirname $0 ); cd ${aCurDir}; aCurDir=$( pwd -P )
- aDirApp=$( echo $aCurDir | awk '{ n = split( $0, a, "/" ); print a[n-1]"/"a[n] }' )
+    aCurDir=$( dirname $0 ); cd ${aCurDir}; aCurDir=$( pwd -P )
+    aDirApp=$( echo ${aCurDir} | awk '{ n = split( $0, a, "/" ); print a[n-1]"/"a[n] }' )
+#   aAppDir=$( echo ${aDirApp} | awk 'BEGIN {IGNORECASE = 1} { sub( /(client|server)\//, "" ); print $0 }' )
 
- echo "" 
- echo "*************************************************************************************************"
- echo ""
- echo "  Copying files from ./${aBuild} to: ${aHost}:/webs/FRApps/${aDirApp}/${aBuild}"
- echo ""
+    echo ""
+    echo "*************************************************************************************************"
+    echo ""
+    echo "  Copying files from ./${aBuild} to: ${aHost}:/webs/FRApps/${aDirApp}/${aBuild}"
+    echo ""
 
- echo "  $  ssh  ${aHost}  mkdir -p ${aWebDir}/${aDirApp}/${aBuild}"
- echo "  $  scp -r build/* ${aHost}:${aWebDir}/${aDirApp}/${aBuild}/"
- echo ""
-            ssh  ${aHost}  mkdir -p ${aWebDir}/${aDirApp}/${aBuild}    >/dev/null 2>&1
-            scp -r build/* ${aHost}:${aWebDir}/${aDirApp}/${aBuild}/
+    echo "  $  ssh  ${aHost}  mkdir -p ${aWebDir}/${aDirApp}/${aBuild}"
+    echo "  $  scp -r build/* ${aHost}:${aWebDir}/${aDirApp}/${aBuild}/"
+    echo ""
+               ssh  ${aHost}  mkdir -p ${aWebDir}/${aDirApp}/${aBuild}    >/dev/null 2>&1
+               scp -r build/* ${aHost}:${aWebDir}/${aDirApp}/${aBuild}/
 
- echo "*************************************************************************************************"
-
-
-
+    echo "*************************************************************************************************"
