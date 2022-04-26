3538e9f3bb243eb26d6fc2e90bb8cf606461c51a
robinmattern <robin.mattern@gmail.com>
2022-04-26 09:55:21 -0400

    Combine 3 widgets into one "app"

---
+++ client/0c-html-home/assets/js/animated-navigation_u2.js
@@ -0,0 +1,70 @@
+//  import  setPanels from './expanding-cards_u6.js';
+
+       var  ToggleButton = document.getElementById( 'toggle' )
+       var  NavMenuBar   = document.getElementById( 'nav' )
+
+// -------------------------------------------------------------------------------------
+
+//function  setBar( ) {
+//          ToggleButton.addEventListener( 'click', ( ) => NavMenuBar.classList.toggle( 'active' ) )
+            ToggleButton.addEventListener( 'click', ToggleButton_onClick )
+//          }
+// ------------------------------------------------------------------------
+
+  function  ToggleButton_onClick( Nav_Event ) {
+            NavMenuBar.classList.toggle( 'active' )
+            }
+// -------------------------------------------------------------------------------------
+
+//function  setItems( ) {
+            NavMenuBar_addClickListeners( )
+//          } 
+//          -------------------------------------------------------
+
+  function  NavMenuBar_addClickListeners( ) {
+       var mNavItems = document.querySelectorAll( 'nav li' )
+           mNavItems.forEach( NavItem_addClickListener )
+            }
+//          -------------------------------------------------------
+
+  function  NavItem_addClickListener( pNavItem ) {
+           pNavItem.addEventListener( 'click', NavItem_onClick )
+            }
+//          -------------------------------------------------------
+
+  function  NavItem_onClick( NavItem_Event ) {
+       var aNavItem  =  NavItem_Event.currentTarget.innerText;
+
+       if (aNavItem == "Contact") { alert( " *** " + aNavItem + " is not defined" ) }
+
+       if (aNavItem == "FAQ"    ) { toggleDiv( '.faq-container' ) }
+
+//     if (aNavItem == "Cards"  ) { toggleDiv( '#cards-container' )
+//                                  setPanels( )
+       if (aNavItem == "Cards"  ) { toggleDiv( '#cards-container', "flex" )
+                                    }
+            } // eof NavItem_onClick
+//          -------------------------------------------------------
+
+  function  toggleDiv( aSelector, aDisplay ) {
+//     var  pDiv        =  document.querySelectorAll( '.faq-container' )[0]
+       var  pDiv        =  document.querySelectorAll( aSelector )[ 0]
+
+//     var  aVisibility =  pDiv.style.visibility
+//          pDiv.style.visibility = (aVisibility == "hidden") ? "visible" :"hidden"
+
+       var  aVisibility =  pDiv.style.display
+//          pDiv.style.display = (aVisibility == "none") ? "block" :"none"
+
+            aDisplay    =  aDisplay ? aDisplay : "block"
+            pDiv.style.display = (aVisibility == "none") ? aDisplay :"none"
+            }
+//          -------------------------------------------------------
+// ------------------------------------------------------------------------
+
+       var  NavMenu = { setBar: {}, setItems: {} }   
+
+   export { NavMenu }            // Named export. Imported name must match. 
+//  export  default NavMenu      // Default exoirt  
+
+// ------------------------------------------------------------------------
---
+++ client/0c-html-home/assets/js/index.js
@@ -0,0 +1,14 @@
+
+// import                    '../../assets/css/animated-navigation.css'
+
+// import { MenuBar }  from  '../../assets/js/animated-navigation_u2.js'
+   import { NavMenu }  from  '../../assets/js/animated-navigation_u2.js'
+//          NavMenu.setBar()     // Executed when loaded (if it were defined) 
+//          NavMenu.setItems() 
+
+   import   setPanels  from  '../../assets/js/expanding-cards_u6.js'
+            setPanels( )         // Not executed when loaded  
+   
+// import   setFAQs    from  '../../assets/js/faq-collapse_u2.js'
+   import                    '../../assets/js/faq-collapse_u2.js'
+   
