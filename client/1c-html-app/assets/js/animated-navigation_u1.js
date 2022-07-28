b3c003d9d5cd0b284272ffba15d1ed14a943225e
robinmattern <robin.mattern@gmail.com>
2022-07-28 11:27:01 -0400

    Added client\1c-html-app back

---
+++ client/1c-html-app/assets/js/animated-navigation_u1.js
@@ -0,0 +1,18 @@
+       var  Toggle_Button = document.getElementById( 'toggle' )
+       var  Nav_MenuBar   = document.getElementById( 'nav' )
+
+//  --------------------------------------------------------------
+
+//function  setNav() {
+//          toggle.addEventListener( 'click', ( ) => Nav_MenuBar.classList.toggle( 'active' ) )
+            Toggle_Button.addEventListener( 'click', Toggle_Button_onClick )
+//          }
+//  --------------------------------------------------------------
+
+//function  Toggle_Button_onClick( Nav_Event ) { 
+  function  Toggle_Button_onClick(  ) { 
+            Nav_MenuBar.classList.toggle( 'active' )
+            }
+//  --------------------------------------------------------------
+
+
