8a10698c36b6062f68d634550128534e6ed54a28
robinmattern <robin.mattern@gmail.com>
2022-10-21 18:23:33 -0400

    Add URLs for each app in README.md

--- README.json.mjs
+++ README.json.mjs
@@ -1,8 +1,8 @@
-//  console.log( require( 'util').inspect( README_JSON(), { depth: 9 } ) )
+//console.log( require( 'util').inspect( README_JSON(), { depth: 9 } ) )
 
   export default README_JSON
 
-function README_JSON() {  
+function README_JSON() {
 return [
 , { proj: "formR's First Sample Apps"
   , name: "FRApps_prod-master", stages: [
@@ -10,90 +10,90 @@ return [
   , { stage: "Client1 HTML Apps", apps: [
 
     , { app: "1c1. My HTML Custom App"
-      , txt: "A simple responsive app using HTML and .CSS styles" 
+      , txt: "A simple responsive app using HTML and .CSS styles"
       , url: 'client1/1c1_my-html-custom-app/index_final_custom.html' }
 
     , { app: "2c1. Student Website"
-      , txt: "A website based on My HTML Custom App" 
+      , txt: "A website based on My HTML Custom App"
       , url: 'client1/2c1_student-website/index.html' }
     ] }
 
   , { stage: "Client2 JavaScript Apps", apps: [
 
     , { app: "1c2. My JavaScript Custom App"
-      , txt: "A refactored JavaScript app using My HTML Custom App" 
-      , url: '' }
+      , txt: "A refactored JavaScript app using My HTML Custom App"
+      , url: 'client2/1c2_my-javascript-custom-app/index.html' }
 
     , { app: "2c2. My JavaScript Click Samples"
-      , txt: "A refactored JavaScript app using My HTML Custom App" 
-      , url: '' }
+      , txt: "A refactored JavaScript app using My HTML Custom App"
+      , url: 'client2/2c2_javascript-click-samples/index.html' }
 
     , { app: "3c2. JavaScript FAQs App"
-      , txt: "A simple JavaScript app to display FAQs using collapsing detail / stage items" 
-      , url: '' }
+      , txt: "A simple JavaScript app to display FAQs using collapsing detail / stage items"
+      , url: 'client2/3c2_javascript-faqs-app/index.html' }
 
     , { app: "4c2. JavaScript Cards App"
-      , txt: "A simple JavaScript app to display cards using a collapsing carousel" 
-      , url: '' }
+      , txt: "A simple JavaScript app to display cards using a collapsing carousel"
+      , url: 'client2/4c2_javascript-cards-app/index.html' }
 
     , { app: "5c2. JavaScript JSON Data App"
-      , txt: "A simple JavaScript app to retreive JSON data requested with an API call." 
-      , url: '' }
+      , txt: "A simple JavaScript app to retreive JSON data requested with an API call."
+      , url: 'client2/5c2_javascript-json-data-app/index.html' }
     ] }
 
   , { stage: "Client3 React Apps", apps: [
 
     , { app: "1c3. React Cards and FAQs App"
-      , txt: "A simple React app to display FAQs using collapsing cards and detail / stage items" 
-      , url: '' }
+      , txt: "A simple React app to display FAQs using collapsing cards and detail / stage items"
+      , url: 'client3/1c3-test-react-empty-app/build/index.html' }
 
     , { app: "2c3. React Empty App"
-      , txt: "A clean React client app with only one line of HTML and all the React dependancies." 
-      , url: '' }
+      , txt: "A clean React client app with only one line of HTML and all the React dependancies."
+      , url: 'client3/2c3_react-faqs-n-cards-app/index.html' }
 
     , { app: "3c3. React Button"
-      , txt: "A simple button componant using React deployed from an Internet CDN." 
-      , url: '' }
+      , txt: "A simple button componant using React deployed from an Internet CDN."
+      , url: 'client3/3c3-react-button/index.html' }
 
     , { app: "4c3. React App with No API"
-      , txt: "A simple React app using client-side routes and data." 
-      , url: '' }
+      , txt: "A simple React app using client-side routes and data."
+      , url: 'client3/4c3-react-app-no-api/build/index.html' }
 
     , { app: "5c3. React App with API"
-      , txt: "A simple React app using client-side routes and data served with an API call." 
-      , url: '' }
+      , txt: "A simple React app using client-side routes and data served with an API call."
+      , url: 'client3/5c3-react-app-wi-api/build/index.html' }
 
     , { app: "6c3. My React Custom App"
-      , txt: "A simple React client app that can be used as a home page template." 
-      , url: '' }
+      , txt: "A simple React client app that can be used as a home page template."
+      , url: 'client3/6c3-react-custom-api/build/index..html' }
     ] }
 
   , { stage: "Client4 Lit Apps", apps: [
 
     , { app: "1c4. Simple Lit Component App"
-      , txt: "A clean Lit component" 
-      , url: '' }
+      , txt: "A clean Lit component"
+      , url: 'client4/1c4_simple-lit-component/index.html' }
 
     , { app: "2c4. Lit Cards and FAQs App"
-      , txt: "A simple Lit app to display FAQs using collapsing cards and detail / stage items" 
-      , url: '' }
+      , txt: "A simple Lit app to display FAQs using collapsing cards and detail / stage items"
+      , url: 'client4/2c4_lit-faqs-n-cards-app/index.html' }
 
     , { app: "3c4. Lit App with API"
-      , txt: "A simple Lit app requesting data from a server API" 
-      , url: '' }
+      , txt: "A simple Lit app requesting data from a server API"
+      , url: 'client4/3c4_lit-app-wi-api/index.html' }
     ] }
 
   , { stage: "Server2 JSON APIs", apps: [
 
     , { app: "5s2. JSON Data API"
-      , txt: "A simple API to return JSON data requested with an API call." 
-      , url: '' }
+      , txt: "A simple API to return JSON data requested with an API call."
+      , url: 'server2/5s2-json-data-api/index.html' }
     ] }
 
   , { stage: "Server3 MySQL APIs", apps: [
 
     , { app: "5s3. React Data API]"
-      , txt: "A simple React API to return data requested with an API call." 
+      , txt: "A simple React API to return data requested with an API call."
       , url: 'server3/5s3-react-app-wi-api/index.html' }
     ] }
   ] }
@@ -104,41 +104,51 @@ return [
   , { stage: "Client React Admin Apps", apps: [
 
     , { app: "1c. World"
-      , txt: "A React client app to view data in the World database." 
-      , url: '' }
+      , txt: "A React client app to view data in the World database."
+      , url: 'client/1c-world/public/index.html' }
 
     , { app: "3c. RAuth"
-      , txt: "A React client app to authenticate users in the RAuth table." 
-      , url: '' }
+      , txt: "A React client app to authenticate users in the RAuth table."
+      , url: 'client/3c-rauth/public/index.html' }
 
     , { app: "4c. Admin"
-      , txt: "A React client app to view, add, edit or delete users in the RAuth table" 
-      , url: '' }
+      , txt: "A React client app to view, add, edit or delete users in the RAuth table"
+      , url: 'client/4c-admin/public/index.html' }
 
     , { app: "5c. formR"
-      , txt: "A React client app to view, add, edit or delete data in the World database" 
-      , url: '' }
+      , txt: "A React client app to view, add, edit or delete data in the World database"
+      , url: 'client/5c-formr/public/index.html' }
     ] }
 
   , { stage: "Server MySQL APIs", apps: [
 
     , { app: "1s. World"
-      , txt: "A React server api to view data in the World database." 
-      , url: '' }
+      , txt: "A React server api to view data in the World database."
+      , url: 'server/1s-world/index.html' }
 
     , { app: "3s. RAuth"
-      , txt: "A React server api to authenticate users in the RAuth table." 
-      , url: '' }
+      , txt: "A React server api to authenticate users in the RAuth table."
+      , url: 'server/3s-rauth/index.html' }
 
     , { app: "4s. Admin"
-      , txt: "A React server api to view, add, edit or delete users in the RAuth table" 
-      , url: '' }
+      , txt: "A React server api to view, add, edit or delete users in the RAuth table"
+      , url: 'server/4s-admin/index.html' }
 
     , { app: "5s. formR"
-      , txt: "A React server api to view, add, edit or delete data in the World database" 
-      , url: '' }
+      , txt: "A React server api to view, add, edit or delete data in the World database"
+      , url: 'server/5s-formr/index.html' }
     ] }
   ] }
 ]
 
 }
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # <u>formR's First Sample Apps</u>
   <h2 style="font-size:24px; margin: -18px 0px 15px 12px;">(FRApps_prod-master)</h2>
 
@@ -12,58 +13,58 @@
   </details>
   <details><summary><b style="font-size:24px;">Client2 JavaScript Apps</b></summary>
 
-  - ### [1c2. My JavaScript Custom App]()
+  - ### [1c2. My JavaScript Custom App](client2/1c2_my-javascript-custom-app/index.html)
     A refactored JavaScript app using My HTML Custom App
 
-  - ### [2c2. My JavaScript Click Samples]()
+  - ### [2c2. My JavaScript Click Samples](client2/2c2_javascript-click-samples/index.html)
     A refactored JavaScript app using My HTML Custom App
 
-  - ### [3c2. JavaScript FAQs App]()
+  - ### [3c2. JavaScript FAQs App](client2/3c2_javascript-faqs-app/index.html)
     A simple JavaScript app to display FAQs using collapsing detail / stage items
 
-  - ### [4c2. JavaScript Cards App]()
+  - ### [4c2. JavaScript Cards App](client2/4c2_javascript-cards-app/index.html)
     A simple JavaScript app to display cards using a collapsing carousel
 
-  - ### [5c2. JavaScript JSON Data App]()
+  - ### [5c2. JavaScript JSON Data App](client2/5c2_javascript-json-data-app/index.html)
     A simple JavaScript app to retreive JSON data requested with an API call.
 
   </details>
   <details><summary><b style="font-size:24px;">Client3 React Apps</b></summary>
 
-  - ### [1c3. React Cards and FAQs App]()
+  - ### [1c3. React Cards and FAQs App](client3/1c3-test-react-empty-app/build/index.html)
     A simple React app to display FAQs using collapsing cards and detail / stage items
 
-  - ### [2c3. React Empty App]()
+  - ### [2c3. React Empty App](client3/2c3_react-faqs-n-cards-app/index.html)
     A clean React client app with only one line of HTML and all the React dependancies.
 
-  - ### [3c3. React Button]()
+  - ### [3c3. React Button](client3/3c3-react-button/index.html)
     A simple button componant using React deployed from an Internet CDN.
 
-  - ### [4c3. React App with No API]()
+  - ### [4c3. React App with No API](client3/4c3-react-app-no-api/build/index.html)
     A simple React app using client-side routes and data.
 
-  - ### [5c3. React App with API]()
+  - ### [5c3. React App with API](client3/5c3-react-app-wi-api/build/index.html)
     A simple React app using client-side routes and data served with an API call.
 
-  - ### [6c3. My React Custom App]()
+  - ### [6c3. My React Custom App](client3/6c3-react-custom-api/build/index..html)
     A simple React client app that can be used as a home page template.
 
   </details>
   <details><summary><b style="font-size:24px;">Client4 Lit Apps</b></summary>
 
-  - ### [1c4. Simple Lit Component App]()
+  - ### [1c4. Simple Lit Component App](client4/1c4_simple-lit-component/index.html)
     A clean Lit component
 
-  - ### [2c4. Lit Cards and FAQs App]()
+  - ### [2c4. Lit Cards and FAQs App](client4/2c4_lit-faqs-n-cards-app/index.html)
     A simple Lit app to display FAQs using collapsing cards and detail / stage items
 
-  - ### [3c4. Lit App with API]()
+  - ### [3c4. Lit App with API](client4/3c4_lit-app-wi-api/index.html)
     A simple Lit app requesting data from a server API
 
   </details>
   <details><summary><b style="font-size:24px;">Server2 JSON APIs</b></summary>
 
-  - ### [5s2. JSON Data API]()
+  - ### [5s2. JSON Data API](server2/5s2-json-data-api/index.html)
     A simple API to return JSON data requested with an API call.
 
   </details>
@@ -79,31 +80,31 @@
 
   <details><summary><b style="font-size:24px;">Client React Admin Apps</b></summary>
 
-  - ### [1c. World]()
+  - ### [1c. World](client/1c-world/public/index.html)
     A React client app to view data in the World database.
 
-  - ### [3c. RAuth]()
+  - ### [3c. RAuth](client/3c-rauth/public/index.html)
     A React client app to authenticate users in the RAuth table.
 
-  - ### [4c. Admin]()
+  - ### [4c. Admin](client/4c-admin/public/index.html)
     A React client app to view, add, edit or delete users in the RAuth table
 
-  - ### [5c. formR]()
+  - ### [5c. formR](client/5c-formr/public/index.html)
     A React client app to view, add, edit or delete data in the World database
 
   </details>
   <details><summary><b style="font-size:24px;">Server MySQL APIs</b></summary>
 
-  - ### [1s. World]()
+  - ### [1s. World](server/1s-world/index.html)
     A React server api to view data in the World database.
 
-  - ### [3s. RAuth]()
+  - ### [3s. RAuth](server/3s-rauth/index.html)
     A React server api to authenticate users in the RAuth table.
 
-  - ### [4s. Admin]()
+  - ### [4s. Admin](server/4s-admin/index.html)
     A React server api to view, add, edit or delete users in the RAuth table
 
-  - ### [5s. formR]()
+  - ### [5s. formR](server/5s-formr/index.html)
     A React server api to view, add, edit or delete data in the World database
 
   </details>
